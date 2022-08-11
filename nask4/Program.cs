14eecaffbeda43a91ae8c8563145be71519c5de5
dipperlost <dipper>
2022-08-11 16:27:01 +0300

    task4

---
+++ nask4/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число А: ");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число B: ");
+        int numberB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число C: ");
+        int numberC = Convert.ToInt32(Console.ReadLine());
+        int max = 0;
+        if (numberA > numberB)
+        {
+            max = numberA;
+        }
+        else
+        {
+            max = numberB;
+        }
+
+        if (numberC > max)
+        {
+            max = numberC;
+        }
+        Console.WriteLine(max);
+    }
+}
