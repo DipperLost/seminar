ffb9d298df91eeb506e342ab05a0011f72abac48
dipperlost <dipper>
2022-08-11 16:18:50 +0300

    task8

---
+++ nask8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if(count < N)
+{
+    while(count <= N)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+}
+else
+{
+    while(count >= N)
+    {
+        Console.WriteLine(N);
+        N++;
+    }
+}
